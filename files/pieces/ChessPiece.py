e9fcc87899707b8a54231dd5cd85dc7ff57d0f18
M3M0KA <koa.balke@hotmail.com>
2025-04-30 22:00:59 +0200

    Alles geht ausser figuren draw

--- files/pieces/ChessPiece.py
+++ files/pieces/ChessPiece.py
@@ -3,7 +3,7 @@ class ChessPiece:
         self.color = color  # 'W' oder 'B'
 
     def is_valid_move(self, start, end, board):
-        raise NotImplementedError
+        raise NotImplementedError("is_valid_move() muss in der Unterklasse implementiert werden.")
 
     def __str__(self):
         return self.symbol + self.color
