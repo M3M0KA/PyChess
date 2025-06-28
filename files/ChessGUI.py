91929a991d84d34a07b6b5e85c52ecf1e040fd23
Koa <koa.balke@hotmail.com>
2025-06-28 22:01:16 +0200

    fenster grösse änderbar

--- files/ChessGUI.py
+++ files/ChessGUI.py
@@ -3,20 +3,21 @@ import os
 from .logic import move_piece, create_board, set_global_variables
 from .pieces import ChessPiece
 
-IMAGES_PATH = os.path.join(os.path.dirname(__file__), 'images')
+IMAGES_PATH = os.path.join(os.path.dirname(__file__), 'temp_images')
 piece_images = {}
 pieces = ['pawn', 'rook', 'knight', 'bishop', 'queen', 'king']
 colors = ['w', 'b']
 
 class ChessGUI:
-    def __init__(self):
+    def __init__(self, windowsize):
+        self.windowsize = windowsize
         self.current_color = 'W'
         self.temp1 = None
         self.load_images()
         self.GREEN = (118, 150, 86)
         self.WHITE = (255, 255, 255)
         pygame.init()
-        self.screen = pygame.display.set_mode((800, 800))
+        self.screen = pygame.display.set_mode((windowsize, windowsize))
         self.screen.fill((234,255,123))
         self.clock = pygame.time.Clock()
         self.clock.tick(60)
@@ -53,28 +54,29 @@ class ChessGUI:
                         image_temp = f"{piece.color.lower()}_{piece.symbol.lower()}"
                         image = piece_images.get(image_temp)
                         if image:
-                            image = pygame.transform.scale(image, (50, 50))
-                            screen.blit(image, (x * 60 + 164, y * 60 + 164))
+                            image = pygame.transform.scale(image, ((0.0625 * self.windowsize), (0.0625 * self.windowsize)))
+                            screen.blit(image, (x * (self.windowsize / 13.33333) + (0.205 * self.windowsize), y * (self.windowsize / 13.33333) + (0.205 * self.windowsize)))
                         
 
     def draw_board(self, screen):
-        square_size = 60  # Grösse Schachfeld
+        square_size = (self.windowsize / 13.33333)  # Grösse Schachfeld
         for y in range(8):
             for x in range(8):
                 color = self.WHITE if (x + y) % 2 == 0 else self.GREEN
-                pygame.draw.rect(screen, color, pygame.Rect(x * square_size + 160, y * square_size + 160, square_size, square_size))
+                pygame.draw.rect(screen, color, pygame.Rect(x * square_size + (self.windowsize / 5), y * square_size + (self.windowsize / 5), square_size, square_size))
 
     def handle_click(self, pos):
         x, y = pos
-        x = (x - 160) // 60
-        y = (y - 160) // 60
+        x = int((x - (self.windowsize/5)) // (0.075 * self.windowsize))
+        y = int((y - (self.windowsize/5)) // (0.075 * self.windowsize))
+        print(x, y)
         if 0 <= x < 8 and 0 <= y < 8:
             if self.temp1 is None:
                 if self.board[y][x] is None or self.board[y][x].color != self.current_color or isinstance(self.board[y][x], ChessPiece) == False:
                     return
                 else:
                     self.temp1 = (x, y)
-                    pygame.draw.rect(self.screen, (255, 0, 0), (x * 60 + 160, y * 60 + 160, 60, 60), 3)
+                    pygame.draw.rect(self.screen, (255, 0, 0), (x * (self.windowsize/13.33333) + (self.windowsize/5), y * (self.windowsize/13.33333) + (self.windowsize/5), (self.windowsize/13.33333), (self.windowsize/13.33333)), 3)
                     pygame.display.flip()
             else:
                 self.temp2 = (x, y)
@@ -96,29 +98,28 @@ class ChessGUI:
             text = font.render("Unentschieden!", True, (0, 0, 0))
         else:
             text = font.render(f"{winner} gewinnt!", True, (0, 0, 0))
-        self.screen.blit(text, (350, 700))
+        self.screen.blit(text, ((0.4375 * self.windowsize), (0.875 * self.windowsize)))
         pygame.display.flip()
         global someone_won
         someone_won = True
 
     def promotion_options(self, screen):
         font = pygame.font.Font(None, 30)
-        self.size_temp = 147.5
         choices = {
-            'Dame': (145, 100),
-            'Turm': (275, 100),
-            'Läufer': (405, 100),
-            'Springer': (535, 100)
+            'Dame': ((0.18125 * self.windowsize), (0.125 * self.windowsize)),
+            'Turm': ((0.34375 * self.windowsize), (0.125 * self.windowsize)),
+            'Läufer': ((0.50625 * self.windowsize), (0.125 * self.windowsize)),
+            'Springer': ((0.66875 * self.windowsize), (0.125 * self.windowsize))
         }   
         for name, pos in choices.items():
             text = font.render(name, True, (0, 0, 0))
-            button = pygame.Rect(pos[0], pos[1], 120, 50)
+            button = pygame.Rect(pos[0], pos[1], (0.15 * self.windowsize), (0.0625 * self.windowsize))
             pygame.draw.rect(screen, (200, 200, 200), button)
-            screen.blit(text, (button.x + 10, button.y + 10))
+            screen.blit(text, (button.x + (0.0125 * self.windowsize), button.y + (0.0125 * self.windowsize)))
         pygame.display.flip()
         
     def wait_for_promotion(self):
-        background = pygame.Rect(0, 100, 1000, 50)
+        background = pygame.Rect(0, (0.125 * self.windowsize), self.windowsize, (0.0625 * self.windowsize))
         waiting = True
         while waiting:
             for event in pygame.event.get():
@@ -126,19 +127,19 @@ class ChessGUI:
                     exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     x, y = event.pos
-                    if x > 145 and x < 265 and y > 100 and y < 150:
+                    if x > (0.18125 * self.windowsize) and x < (0.33125 * self.windowsize) and y > (self.windowsize * 0.125) and y < (0.1875 * self.windowsize):
                         pygame.draw.rect(self.screen, (234, 255, 123), background)
                         pygame.display.flip()
                         return 'Dame'
-                    if x > 275 and x < 395 and y > 100 and y < 150:
+                    if x > (0.34375 * self.windowsize) and x < (0.49375 * self.windowsize) and y > (self.windowsize * 0.125) and y < (0.1875 * self.windowsize):
                         pygame.draw.rect(self.screen, (234, 255, 123), background)
                         pygame.display.flip()
                         return 'Turm'
-                    if x > 405 and x < 525 and y > 100 and y < 150:
+                    if x > (0.50625 * self.windowsize) and x < (0.65625 * self.windowsize) and y > (self.windowsize * 0.125) and y < (0.1875 * self.windowsize):
                         pygame.draw.rect(self.screen, (234, 255, 123), background)
                         pygame.display.flip()
                         return 'Läufer'
-                    if x > 535 and x < 655 and y > 100 and y < 150:
+                    if x > (0.66875 * self.windowsize) and x < (0.81875 * self.windowsize) and y > (self.windowsize * 0.125) and y < (0.1875 * self.windowsize):
                         pygame.draw.rect(self.screen, (234, 255, 123), background)
                         pygame.display.flip()
                         return 'Springer'
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from files.ChessGUI import ChessGUI
+from files.images import image_editor
+
 
 if __name__ == "__main__":
-    gui = ChessGUI()
+    while True:
+        try:
+            windowsize = int(input("\nBitte Fenstergrösse angeben(in px): "))
+            if windowsize >= 50 and windowsize <= 2000:
+                break
+            else:
+                print("\nBitte eine Zahl zwischen 50 und 2000.")
+        except ValueError:
+            print("\nBitte Zahl angeben! (zwischen 50 und 2000)")
+    editor = image_editor(windowsize)
+    editor.create_copys()
+    editor.resize()
+
+    gui = ChessGUI(windowsize)
     gui.run()
